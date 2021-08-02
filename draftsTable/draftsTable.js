f84164bc87c48762f1ab6e435392e87059c431dc
Nelson/Roberto <hello@nelsonroberto.com>
2021-08-02 09:33:39 -0400

    Base IDs are now stored in json file
    A list of Based ID's can be stored in a json file instead of in the code. This allows Bases to be referenced by name.

--- draftsTable/draftsTable.js
+++ draftsTable/draftsTable.js
@@ -1,15 +1,15 @@
 // draftsTable v0.1
 class Airtable {
-  constructor(baseName, tableName) {
-    this.baseLookup = {
-      //"My Table": "wXyA4nkuMtT0U",
-    };
+  constructor(tableName, baseName = "default") {
+    this._authorize();
     this.endPointURL = "https://api.airtable.com/v0/";
-    this.baseID = this.baseLookup[baseName];
+    // The `/` path corresponds to the `Drafts` folder in iCloud
+    this.filesPath = "/";
+    this.baseID = this._baseLookup(baseName);
     this.tableName = tableName;
     this.parameters = {};
     this.records = [];
-    this._authorize();
+    this.testRecord = {};
   }
 
   addParameter(key, value) {
@@ -119,87 +119,22 @@ class Airtable {
   _authorize() {
     var credential = Credential.create("AirTable", "AirTable API");
     credential.addPasswordField("api_key", "API Key");
-    credential.addTextField("baseID", "Base ID");
+    credential.addTextField("baseID", "Default Base ID");
     credential.authorize();
     this.api_key = credential.getValue("api_key");
-    //this.baseID = credential.getValue("baseID");
-  }
-}
-
-// Old Code
-function airTableReadData(fields, filter, maxRecords, view) {
-  // Basic variables
-  var endpoint = "https://api.airtable.com/v0/";
-  var testData = {
-    records: [
-      {
-        id: "rec7j1okh61sUqMy1",
-        fields: {
-          UlyssesID: "VJ6rT9dJdrW2BYk2unta4A",
-          Status: "Writing",
-          Title: "Ghost: Customer Support Engineer",
-        },
-        createdTime: "2021-07-28T11:36:02.000Z",
-      },
-      {
-        id: "recPxTZe69Kd5VTIE",
-        fields: {
-          Status: "Writing",
-          Title:
-            "Perchance to Dream, Stories about living a life detached from reality",
-        },
-        createdTime: "2021-07-28T10:26:09.000Z",
-      },
-      {
-        id: "reclcZoMA95nCxNFX",
-        fields: { Status: "Writing", Title: "Fact or Fiction" },
-        createdTime: "2021-07-28T10:26:09.000Z",
-      },
-      {
-        id: "recoew3RZl5LHipse",
-        fields: { Status: "Writing", Title: "What to cut, what to keep" },
-        createdTime: "2021-07-28T10:26:09.000Z",
-      },
-    ],
-  };
-
-  // Setup & Store Credentials
-  var credential = Credential.create("AirTable", "AirTable API");
-  credential.addPasswordField("api_key", "API Key");
-  credential.addTextField("baseID", "Base ID");
-  credential.authorize();
-  var secret = credential.getValue("api_key");
-  var baseID = credential.getValue("baseID");
-
-  // Create URL
-  url = endpoint + baseID + "/Content";
-  var http = HTTP.create(); // create HTTP object
-  var params = {
-    maxRecords: maxRecords,
-    filterByFormula: filter,
-    "Fields[]": fields,
-    view: view,
-  };
-  var response = http.request({
-    url: url,
-    method: "GET",
-    parameters: params,
-    headers: {
-      Authorization: "Bearer " + secret,
-    },
-  });
-
-  console.log(response);
-
-  if (response.success) {
-    var text = response.responseText;
-    var data = response.responseData;
-    var results = JSON.parse(text);
-  } else {
-    console.log(response.statusCode);
-    console.log(response.error);
+    this.baseID = credential.getValue("baseID");
   }
 
-  return results;
-  //return testData;
+  // Allows BaseIDs to be stored in an icloud json file.
+  // Default base id is used if no name is specified
+  _baseLookup(baseName) {
+    if (baseName == "default") {
+      return this.defaultBaseID;
+    } else {
+      // read from file in iCloud
+      let fmCloud = FileManager.createCloud(); // Connect to iCloud storage
+      let bases = fmCloud.readJSON(this.filesPath + "bases.json");
+      return bases[baseName];
+    }
+  }
 }
