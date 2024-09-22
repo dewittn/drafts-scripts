04c3270768c1da516d5206168ef97cb400bf49d1
Nelson de Witt <git@nelsonroberto.com>
2024-09-22 12:14:47 -0400

    feat: migrate scripts to use ActionMenu

---
+++ Library/Scripts/nr/actions/file-as.js
@@ -0,0 +1,4 @@
+require("libraries/ActionMenu.js");
+
+const actionMenu = ActionMenu.createFromGroup("File As Actions");
+actionMenu.select();
---
+++ Library/Scripts/nr/actions/insert-template.js
@@ -0,0 +1,4 @@
+require("libraries/ActionMenu.js");
+
+const actionMenu = ActionMenu.createFromGroup("Template Actions");
+actionMenu.select();
