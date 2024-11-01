736d94c5ed2bb49a5673a7f4843d1054e11e10d9
Nelson de Witt <git@nelsonroberto.com>
2024-11-01 16:24:50 -0400

    feat: migrate season data

--- Library/Scripts/bvr/Season.js
+++ Library/Scripts/bvr/Season.js
@@ -15,12 +15,16 @@ class Season {
 
     this.#bvr = dependancies.bvr;
     this.#team = dependancies.team;
-    this.#tmplSettings = dependancies.tmplSettings;
+    this.#tmplSettings = this.templateSettings;
     this.#schoolSportingHistory = new DataFile(this.recordsFile);
   }
 
   get ui() {
     return this.#bvr.ui;
+  }
+
+  get templateSettings() {
+    return this.#team.templateSettings;
   }
 
   get globalTemplateTags() {
@@ -59,8 +63,8 @@ class Season {
   }
 
   get currentSeasonData() {
-    if (typeof this.teamHistory[this.currentSeasonID] == "array")
-      this.migrateCurrentSeason();
+    // if (typeof this.teamHistory[this.currentSeasonID] == "array")
+    //   this.migrateSeasonData();
 
     return this.teamHistory[this.currentSeasonID];
   }
@@ -71,33 +75,23 @@ class Season {
   }
 
   set currentSeasonRecord(arry) {
-    this.currentSeasonData.seasonRecordData = arry;
+    this.currentSeasonData.record = arry;
   }
 
   get seasonRecordDraftID() {
     return this.teamHistory[this.currentSeasonID]?.seasonRecordDraftID;
   }
 
-  get gblTmplSettings() {
-    return this.#team.gblTmplSettings;
+  get recordRowTmplSettings() {
+    return this.#tmplSettings.recordRow;
   }
 
-  get tmplSettingsSeasonRecord() {
-    return new TmplSettings(
-      this.gblTmplSettings,
-      this.#tmplSettings.seasonRecord
-    );
+  get seasonRecordTmplSettings() {
+    return this.#tmplSettings.seasonRecord;
   }
 
-  // **********************
-  // Public Functions
-  // **********************
-
-  updateWith(game) {
-    if (game == undefined)
-      this.ui.displayAppMessage("error", "Game data missing!");
-    game.appended = this.#appendToRecordsDraft(game);
-    this.#saveRecordsData(game);
+  updateWith(gameData) {
+    if (gameData.recorded != true) return false;
   }
 
   migrateCurrentSeason() {
@@ -108,16 +102,6 @@ class Season {
       this.#populateSeasonRecordDraft(this.currentSeasonID);
     }
   }
-
-  startNewSeason() {
-    if (this.seasonRecordDraftID == undefined) {
-      this.#createSeasonRecordDraft(this.currentSeasonID);
-    }
-  }
-
-  // **********************
-  // Private Functions
-  // **********************
 
   #migrateSeasonData(seasonID) {
     if (seasonID == undefined) return;
@@ -145,17 +129,21 @@ class Season {
   #createSeasonRecordDraft(seasonID) {
     if (seasonID == undefined) return;
 
-    const seasonRecordDraft = new Template(this.tmplSettingsSeasonRecord);
+    const tmplSettings = this.#generateTmplSettings(
+      this.seasonRecordTmplSettings
+    );
+    const seasonRecordDraft = new Template(tmplSettings);
     seasonRecordDraft.archive().save();
+    this.ui.debugVariable(seasonRecordDraft.draft);
 
-    if (this.teamHistory[seasonID] == undefined)
-      this.teamHistory[seasonID] = {};
     this.teamHistory[seasonID].seasonRecordDraftID = seasonRecordDraft.draftID;
     this.#schoolSportingHistory.save();
 
     return this.ui.displayAppMessage("success", "Season record draft created.");
   }
 
+  //  teamHistory -> seasonData -> recordDraftID
+  //  teamHistory -> seasonData -> recordData
   #populateSeasonRecordDraft(seasonID) {
     this.currentSeasonRecord.forEach((gameReport) => {
       const gameData = this.#loadGameDataFromUUID(gameReport.draftID);
@@ -167,42 +155,26 @@ class Season {
     );
   }
 
-  #appendToRecordsDraft(game) {
-    if (game.appended == true) return true;
-    if (
-      this.seasonRecordDraftID == undefined ||
-      game.tmplSettingsRecordRow == undefined
-    ) {
-      this.ui.displayAppMessage("error", "Game could not be appended.");
-      return false;
-    }
+  #appendToRecordsDraft(gameData) {
+    if (this.seasonRecordDraftID == undefined || gameData == undefined)
+      return alert("Game could not be appended.");
 
     if (this.recordsDraft == undefined)
       this.recordsDraft = Draft.find(this.seasonRecordDraftID);
 
-    const newTableLine = new Template(game.tmplSettingsRecordRow);
+    const tmplSettings = this.#generateTmplSettings(
+      this.recordRowTmplSettings,
+      gameData
+    );
+    const newTableLine = new Template(tmplSettings);
+
     this.recordsDraft.append(newTableLine.content);
     this.recordsDraft.update();
-    return true;
   }
 
-  #saveRecordsData(game) {
-    if (game == undefined)
-      return this.ui.displayAppMessage(
-        "error",
-        "Season record could not be saved."
-      );
-
-    const index = this.currentSeasonRecord.findIndex(
-      (recordItem) => recordItem.draftID == game.draftID
-    );
-
-    if (index !== -1) {
-      this.currentSeasonRecord[index] = game.toJSON();
-    } else {
-      this.currentSeasonRecord = [...this.currentSeasonRecord, game.toJSON()];
-    }
-
+  #saveRecordsData() {
+    this.#gameData.descrition = this.description;
+    this.currentSeasonRecord = [...this.seasonRecord, this.#gameData];
     this.#schoolSportingHistory.save();
   }
 
@@ -216,5 +188,32 @@ class Season {
     data.date = new Date(data.date);
 
     return data;
+  }
+
+  #generateTmplSettings(settings = {}, gameData) {
+    const templateSettings = {
+      ...settings,
+      templateTags: {
+        ...this.globalTemplateTags,
+        team_name: this.teamName,
+        current_season: `${this.currentSeasonID}`,
+        game_date: gameData?.formattedDate,
+        game_location: gameData?.location,
+        game_opponent: gameData?.opponent,
+        game_result: gameData?.result,
+        game_score: gameData?.fullScore,
+        game_score_us: gameData?.score[0],
+        game_score_them: gameData?.score[1],
+        game_description: gameData?.description,
+        game_summary: gameData?.summary,
+        game_highlights: gameData?.highlights,
+        game_comments: gameData?.comments,
+      },
+    };
+    templateSettings.draftTags =
+      templateSettings.draftTags == undefined
+        ? this.defaultDraftTags
+        : [...this.defaultDraftTags, ...templateSettings.draftTags];
+    return templateSettings;
   }
 }
--- Library/Templates/bvr/game-season-record.md
+++ Library/Templates/bvr/game-season-record.md
@@ -1,4 +1,4 @@
-# Season Record: [[team_abbr]] [[current_season]] 
+# [[team_name]]: [[current_season]] Season Record
 [[BVR Coaching]]
 
 | Date | Team | Location | Result | Us | Them |
