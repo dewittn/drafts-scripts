6ed59586683dc9db2a88c8de2da405c1dc6cb4b9
Nelson/Roberto <git@nelsonroberto.com>
2025-04-04 10:40:44 -0400

    ✨ updates to gform script

--- gform-ids.sh
+++ gform-ids.sh
@@ -3,13 +3,23 @@
 ## Supplied URL must be wrapped in quotes.
 ## ./gform-ids.sh "https://docs.google.com/"
 
+function yes_or_no {
+    while true; do
+        read -p "$* [y/n]: " yn
+        case $yn in
+            [Yy]*) return 0  ;;  
+            [Nn]*) echo "Aborted" ; return  1 ;;
+        esac
+    done
+}
+
 FILE="gform.html"
-if [ -z "$1" ]
+if [ ! -f $FILE ]
   then
-    echo "No url supplied."
+    echo "gform.html is missing!."
     exit 1
 fi
 
-curl $1 -o $FILE
 perl -nle 'print "$1 | $2 " while(/[0-9]{8,10}\,"(.*?)",.*?\[\[([0-9]{8,12})/g)' $FILE
-rm $FILE
+
+yes_or_no "Remove gform.html?" && rm $FILE
