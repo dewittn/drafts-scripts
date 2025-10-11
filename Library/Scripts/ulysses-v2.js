02c9258b14cff940547b230c3502d589d784ce3e
Nelson/Roberto <git@nelsonroberto.com>
2025-10-11 15:53:17 -0400

    🧹Remove ulysses-v2 wrapper

--- Library/Scripts/ulysses-v2.js
+++
@@ -1,3 +0,0 @@
-// DEPRECATED: This file has moved. Remove this linking file after migration is complete.
-require("shared/libraries/ulysses-v2.js");
-alert("File 'Scripts/ulysses-v2.js' has moved to 'Scripts/shared/libraries/ulysses-v2.js'. Please update your Drafts action");
